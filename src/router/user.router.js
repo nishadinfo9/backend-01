e3c6130ed401dcbf45c1418c9ce5041228800c9a
nishadinfo09 <mdnishadali03@gmail.com>
2025-07-25 17:57:58 +0600

    add: create models for comments, likes, playlists, and tweets; update user router with new endpoints

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -27,16 +27,19 @@ router.route("/register").post(
 
 router.route("/login").post(loggedInUser);
 
-
 // secure route //
 router.route("/logout").post(jwtToken, loggedOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/password-changes").post(jwtToken, changeCurrentPassword);
 router.route("/current-user").get(jwtToken, getCurrentUser);
-router.route("/update-account").patch(jwtToken, updateAccountDetails)
-router.route("/avatar").patch(jwtToken, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(jwtToken, upload.single("coverImage"), updateUserCoverImage)
-router.route("/c/:username").get(jwtToken, getUserChannelProfile)
-router.route("/history").get(jwtToken, getWatchHistory)
+router.route("/update-account").patch(jwtToken, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(jwtToken, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(jwtToken, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(jwtToken, getUserChannelProfile);
+router.route("/history").get(jwtToken, getWatchHistory);
 
 export default router;
